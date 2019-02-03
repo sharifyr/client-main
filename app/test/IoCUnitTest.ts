15f641e309da799d22ec7a2da43eb1cb5eaba547
Kyle Galvin <kylethedeveloper@gmail.com>
2019-02-03 15:47:14 -0800

    test ioc registry was being used all along

--- app/test/IoCUnitTest.ts
+++ app/test/IoCUnitTest.ts
@@ -3,6 +3,8 @@ import { UserService } from "../src/services/user";
 import { IUserService } from "../src/services/IUserService";
 import { UserClient } from "./mocks/userClient";
 import { IUserClient } from "../src/clients/IUserClient";
+import { IUserReducer, UserReducer } from "../src/reducers/UserReducer";
+import { IFormReducer, FormReducer } from "../src/reducers/FormReducer";
 import { IHttp } from "../src/utils/IHttp";
 import { Http } from "../src/utils/http";
 import { ILogger } from "../src/utils/ILogger";
@@ -11,6 +13,8 @@ import { Logger } from "./mocks/Logger";
 export const configure = () => {
   Container.bind(IUserService).to(UserService);
   Container.bind(IUserClient).to(UserClient);
-  Container.bind(IHttp).to(Http);
+  Container.bind(IUserReducer).to(UserReducer);
+  Container.bind(IFormReducer).to(FormReducer);
   Container.bind(ILogger).to(Logger);
+  Container.bind(IHttp).to(Http);
 };
