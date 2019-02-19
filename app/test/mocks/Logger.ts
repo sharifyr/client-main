717b06011f9a86d8e1b859a66410e958ae44c388
Kyle Galvin <kylethedeveloper@gmail.com>
2019-02-18 16:57:34 -0800

    log cleanup

--- app/test/mocks/Logger.ts
+++ app/test/mocks/Logger.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as bunyan from "bunyan";
+import * as util from "util";
 const bunyanLumberjack = require("bunyan-lumberjack");
 
 import { ILogger } from "../../src/utils/ILogger";
@@ -7,18 +8,18 @@ import { ILogger } from "../../src/utils/ILogger";
 export class Logger implements ILogger {
 
   public info = (...args: any[]) => {
-    console.log("INFO:",args);
+    console.log("INFO:",util.inspect(args));
   }
 
   public warn = (...args: any[]) => {
-    console.log("WARN:",args);
+    console.log("WARN:", util.inspect(args));
   }
 
   public debug = (...args: any[]) => {
-    console.log("DEBUG:",args);
+    console.log("DEBUG:",util.inspect(args));
   }
 
   public error = (...args: any[]) => {
-    console.log("ERROR:",args);
+    console.log("ERROR:",util.inspect(args));
   }
 }
